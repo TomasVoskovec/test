9847e3fc3759c3182c9f8bf16043615800ef9760
voskoto16 <voskoto16@sps-prosek.cz>
2018-09-20 15:00:07 +0200

    closes #4

--- ConsoleApp1/ConsoleApp1/Map.cs
+++ ConsoleApp1/ConsoleApp1/Map.cs
@@ -6,11 +6,7 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    class Program
+    class Map
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Lodě");
-        }
     }
 }
